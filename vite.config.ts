cc4443afcabbf6df0f9242487db0ca19e4bda606
fwio <624835339@qq.com>
2024-09-13 20:58:59 +0800

    fix: netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { vitePlugin as remix } from '@remix-run/dev'
 import { defineConfig } from 'vite'
 import { installGlobals } from '@remix-run/node'
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
+import tsconfigPaths from "vite-tsconfig-paths"
 
 installGlobals()
 
 export default defineConfig({
-  plugins: [remix()],
+  plugins: [remix(), netlifyPlugin(), tsconfigPaths()],
   resolve: {
     alias: {
       zhlint: 'zhlint/dist/zhlint.es.js',
